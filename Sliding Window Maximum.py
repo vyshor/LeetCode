bd9cd5859192cb1dffe9125cafa25249749281c3
vyshor <vyshornehc@gmail.com>
2023-08-16 18:50:57 +0800

    Add alternative to Sliding Window Maximum

--- Sliding Window Maximum.py
+++ Sliding Window Maximum.py
@@ -20,3 +20,54 @@ class Solution:
 # Memory Usage: 26.5 MB, less than 27.90% of Python3 online submissions for Sliding Window Maximum.
 # Time: O(n)
 # Space: O(k)
+
+# class Solution:
+#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+#         n = len(nums)
+#         q = [-1 * num for num in nums[:k-1]]
+#
+#         heapq.heapify(q)
+#         sliding = {}
+#
+#         for num in q:
+#             if num not in sliding:
+#                 sliding[num] = 1
+#             else:
+#                 sliding[num] += 1
+#
+#         ans = []
+#         for i in range(k-1, n):
+#             num = nums[i] * -1
+#             heapq.heappush(q, num)
+#
+#             # print(q)
+#
+#             if num not in sliding:
+#                 sliding[num] = 1
+#             else:
+#                 sliding[num] += 1
+#
+#             maxx = q[0]
+#             while maxx not in sliding:
+#                 heapq.heappop(q)
+#                 maxx = q[0]
+#
+#             if maxx in sliding:
+#                 ans.append(maxx * -1)
+#
+#             # print(sliding)
+#
+#             prev = nums[i-k+1] * -1
+#             sliding[prev] -= 1
+#             if sliding[prev] == 0:
+#                 del sliding[prev]
+#
+#             maxx = q[0]
+#             while maxx not in sliding:
+#                 heapq.heappop(q)
+#
+#                 if not q:
+#                     break
+#                 maxx = q[0]
+#
+#         return ans
