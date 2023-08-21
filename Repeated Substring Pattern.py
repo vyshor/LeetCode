c5f16ca3a4521663d525b35a96df7fc5b9dd3b9a
vyshor <vyshornehc@gmail.com>
2023-08-21 17:25:33 +0800

    Add rotation check for repeated substring pattern

--- Repeated Substring Pattern.py
+++ Repeated Substring Pattern.py
@@ -1,10 +1,21 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         n = len(s)
-        if n == 1:
-            return False
-
-        for start in range(n//2+(n%2)):
-            if n % (start+1) == 0 and s[:start+1] * (n // (start+1)) == s:
+        rotated = s
+        for _ in range(n - 1):
+            rotated = rotated[1:] + rotated[0]
+            if s == rotated:
                 return True
         return False
+
+
+# class Solution:
+#     def repeatedSubstringPattern(self, s: str) -> bool:
+#         n = len(s)
+#         if n == 1:
+#             return False
+#
+#         for start in range(n//2+(n%2)):
+#             if n % (start+1) == 0 and s[:start+1] * (n // (start+1)) == s:
+#                 return True
+#         return False
