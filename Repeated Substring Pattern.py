1c794da164af72681af48812c28280462fe294ea
vyshor <vyshornehc@gmail.com>
2023-08-21 17:38:35 +0800

    Add KMP solution

--- Repeated Substring Pattern.py
+++ Repeated Substring Pattern.py
@@ -1,12 +1,33 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         n = len(s)
-        rotated = s
-        for _ in range(n - 1):
-            rotated = rotated[1:] + rotated[0]
-            if s == rotated:
-                return True
-        return False
+        if s == 1:
+            return False
+
+        lps = [0] * n
+        i, j = 0, 1
+        while j < n:
+            if s[i] == s[j]:
+                lps[j] = i + 1
+                i += 1
+                j += 1
+            else:
+                if i > 0:
+                    i = lps[i - 1]
+                else:
+                    j += 1
+
+        return lps[-1] and (n % (n - lps[-1]) == 0)
+
+# class Solution:
+#     def repeatedSubstringPattern(self, s: str) -> bool:
+#         n = len(s)
+#         rotated = s
+#         for _ in range(n - 1):
+#             rotated = rotated[1:] + rotated[0]
+#             if s == rotated:
+#                 return True
+#         return False
 
 
 # class Solution:
