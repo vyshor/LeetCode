d0c5e50fa624a028b238fdc8c1c3bb0e3759c1a1
vyshor <vyshornehc@gmail.com>
2023-09-09 18:03:31 +0800

    Add updated solutiion for combination sum 4

--- Combination Sum IV.py
+++ Combination Sum IV.py
@@ -1,18 +1,43 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        try:
-            nums = [x for x in nums if x <= target]
-            dp = [0] * (target + 1)
-            for num in nums:
-                dp[num] = 1
+        n = len(nums)
+        q = [-target]
+        dp = {-target: 1}
+        ans = 0
 
-            for i in range(target + 1):
-                for num in nums:
-                    if i - num >= 0:
-                        dp[i] += dp[i - num]
-            return dp[target]
-        except:
-            return 0
+        while q:
+            v = heapq.heappop(q)
+            count = dp[v]
+
+            for num in nums:
+                summ = num + v
+                if summ < 0:
+                    if summ not in dp:
+                        dp[summ] = count
+                        heapq.heappush(q, summ)
+                    else:
+                        dp[summ] += count
+
+                elif summ == 0:
+                    ans += count
+
+        return ans
+
+# class Solution:
+#     def combinationSum4(self, nums: List[int], target: int) -> int:
+#         try:
+#             nums = [x for x in nums if x <= target]
+#             dp = [0] * (target + 1)
+#             for num in nums:
+#                 dp[num] = 1
+#
+#             for i in range(target + 1):
+#                 for num in nums:
+#                     if i - num >= 0:
+#                         dp[i] += dp[i - num]
+#             return dp[target]
+#         except:
+#             return 0
 
 
 # Runtime: 36 ms, faster than 94.47% of Python3 online submissions for Combination Sum IV.
