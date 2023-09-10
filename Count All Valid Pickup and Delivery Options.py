dd2f0868bf70179702278c666de02f7c5a879505
vyshor <vyshornehc@gmail.com>
2023-09-10 23:49:32 +0800

    Add Valid Pickups and Delivery Options

---
+++ Count All Valid Pickup and Delivery Options.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countOrders(self, n: int) -> int:
+        dp = {(0, 0): 1, (1, 0): 1, (0, 1): 1}
+
+        def stateMultiple(pickups, deliveries):
+            if (pickups, deliveries) in dp:
+                return dp[(pickups, deliveries)]
+
+            pickup = 0
+            if pickups > 0:
+                pickup = pickups * stateMultiple(pickups - 1, deliveries + 1)
+
+            delivery = 0
+            if deliveries > 0:
+                delivery = deliveries * stateMultiple(pickups, deliveries - 1)
+
+            dp[(pickups, deliveries)] = (pickup + delivery) % (10 ** 9 + 7)
+
+            return dp[(pickups, deliveries)]
+
+        ans = stateMultiple(n, 0)
+        # print(dp)
+        return ans
+
