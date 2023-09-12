0caa042d3de8b72874146e950b184016500831a2
vyshor <vyshornehc@gmail.com>
2023-09-12 14:00:28 +0800

    Add better solution for H index

--- H-Index.py
+++ H-Index.py
@@ -1,11 +1,29 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         n = len(citations)
-        citations.sort()
+        dp = [0] * (n+1)
 
-        h = 0
-        for i in range(n - 1, -1, -1):
-            if citations[i] >= n - i:
-                h = max(h, n - i)
+        for citation in citations:
+            dp[min(citation, n)] += 1
 
-        return h
+        if dp[n] == n:
+            return n
+
+        for i in range(n-1, -1, -1):
+            dp[i] += dp[i+1]
+            if dp[i] >= i:
+                return i
+
+        return 1
+
+# class Solution:
+#     def hIndex(self, citations: List[int]) -> int:
+#         n = len(citations)
+#         citations.sort()
+#
+#         h = 0
+#         for i in range(n - 1, -1, -1):
+#             if citations[i] >= n - i:
+#                 h = max(h, n - i)
+#
+#         return h
