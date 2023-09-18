7300dbf66aa4fc751cb3e78440287507be5717a2
vyshor <vyshornehc@gmail.com>
2023-09-18 20:18:57 +0800

    Add Update Solution

--- Sum of Number and Its Reverse.py
+++ Sum of Number and Its Reverse.py
@@ -1,25 +1,32 @@
 class Solution:
     def sumOfNumberAndReverse(self, num: int) -> bool:
-        num_str = str(num)
-        n = len(num_str)
-        path = []
+        for i in range(num // 2, num+1):
+            if i + int(str(i)[::-1]) == num:
+                return True
+        return False
 
-        def explorePath(i):
-            for j in range(10):
-                path.append(str(j))
-                # print(path)
-
-                if i + 1 == n:
-                    current_int = int(''.join(path))
-                    ans = current_int + int(str(current_int)[::-1]) == num
-                    if ans:
-                        return True
-                else:
-                    prevPath = explorePath(i + 1)
-                    if prevPath:
-                        return prevPath
-
-                path.pop()
-            return False
-
-        return explorePath(0)
+# class Solution:
+#     def sumOfNumberAndReverse(self, num: int) -> bool:
+#         num_str = str(num)
+#         n = len(num_str)
+#         path = []
+#
+#         def explorePath(i):
+#             for j in range(10):
+#                 path.append(str(j))
+#                 # print(path)
+#
+#                 if i + 1 == n:
+#                     current_int = int(''.join(path))
+#                     ans = current_int + int(str(current_int)[::-1]) == num
+#                     if ans:
+#                         return True
+#                 else:
+#                     prevPath = explorePath(i + 1)
+#                     if prevPath:
+#                         return prevPath
+#
+#                 path.pop()
+#             return False
+#
+#         return explorePath(0)
