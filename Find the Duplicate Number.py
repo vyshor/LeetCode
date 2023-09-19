487ca6634003ee394ede7e231a551ea5455e6f25
vyshor <vyshornehc@gmail.com>
2023-09-20 00:45:31 +0800

    Add alt solution

--- Find the Duplicate Number.py
+++ Find the Duplicate Number.py
@@ -14,3 +14,17 @@ class Solution:
             slower = nums[slower]
 
         return slower
+
+# class Solution:
+#     def findDuplicate(self, nums):
+#         n = len(nums)
+#         ans = 0
+#         for bitcount in range(32):
+#             actual, expected = 0, 0
+#             for i in range(n):
+#                 actual += (nums[i] >> bitcount) % 2
+#                 expected += (i >> bitcount) % 2
+#             if actual > expected:
+#                 ans |= 1 << bitcount
+#
+#         return ans
