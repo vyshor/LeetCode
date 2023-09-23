fc2b9bf163968e333c422e4b29c2a52b5a929e17
vyshor <vyshornehc@gmail.com>
2023-09-23 19:50:20 +0800

    updaate is subseuqnece

--- Is Subsequence.py
+++ Is Subsequence.py
@@ -16,3 +16,21 @@ class Solution:
                 j += 1
         return i == ns
 
+
+# class Solution:
+#     def isSubsequence(self, s: str, t: str) -> bool:
+#         if not s:
+#             return True
+#         if not t and s:
+#             return False
+#
+#         ns = len(s)
+#         nt = len(t)
+#         i, j = 0, 0
+#         while i < ns and j < nt:
+#             if s[i] == t[j]:
+#                 i += 1
+#                 j += 1
+#             else:
+#                 j += 1
+#         return i == ns
