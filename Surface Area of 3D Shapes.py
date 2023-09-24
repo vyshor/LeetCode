a6d2c18749e4fb6efb0ce979666997e44acd122b
vyshor <vyshornehc@gmail.com>
2023-09-24 19:55:45 +0800

    Add optimized code

--- Surface Area of 3D Shapes.py
+++ Surface Area of 3D Shapes.py
@@ -2,33 +2,50 @@ class Solution:
     def surfaceArea(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        total_count = 0
+        count = 0
 
-        bottom = 0
-        count_bottom = True
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]:
+                    count += grid[i][j] * 4 + 2
+                    for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                        if 0 <= x < m and 0 <= y < n:
+                            count -= min(grid[x][y], grid[i][j])
 
-        next_level = True
-        while next_level:
-            next_level = False
-            new_grid = [[0] * n for _ in range(m)]
+        return count
 
-            for i in range(m):
-                for j in range(n):
-                    if grid[i][j]:
-                        if count_bottom:
-                            bottom += 1
 
-                        next_level = True
-                        for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-                            if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] == 0:
-                                total_count += 1
-                        new_grid[i][j] = grid[i][j] - 1
-
-            if count_bottom:
-                total_count += bottom * 2
-                count_bottom = False
-
-            grid = new_grid
-
-        return total_count
+# class Solution:
+#     def surfaceArea(self, grid: List[List[int]]) -> int:
+#         m = len(grid)
+#         n = len(grid[0])
+#         total_count = 0
+#
+#         bottom = 0
+#         count_bottom = True
+#
+#         next_level = True
+#         while next_level:
+#             next_level = False
+#             new_grid = [[0] * n for _ in range(m)]
+#
+#             for i in range(m):
+#                 for j in range(n):
+#                     if grid[i][j]:
+#                         if count_bottom:
+#                             bottom += 1
+#
+#                         next_level = True
+#                         for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+#                             if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] == 0:
+#                                 total_count += 1
+#                         new_grid[i][j] = grid[i][j] - 1
+#
+#             if count_bottom:
+#                 total_count += bottom * 2
+#                 count_bottom = False
+#
+#             grid = new_grid
+#
+#         return total_count
 
