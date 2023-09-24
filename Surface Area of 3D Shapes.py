bc2ef0e48afb2b226b5314926ea749fb70616317
vyshor <vyshornehc@gmail.com>
2023-09-24 19:47:02 +0800

    Add Surface Area of 3D shapes

---
+++ Surface Area of 3D Shapes.py
@@ -0,0 +1,34 @@
+class Solution:
+    def surfaceArea(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        total_count = 0
+
+        bottom = 0
+        count_bottom = True
+
+        next_level = True
+        while next_level:
+            next_level = False
+            new_grid = [[0] * n for _ in range(m)]
+
+            for i in range(m):
+                for j in range(n):
+                    if grid[i][j]:
+                        if count_bottom:
+                            bottom += 1
+
+                        next_level = True
+                        for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                            if x < 0 or x >= m or y < 0 or y >= n or grid[x][y] == 0:
+                                total_count += 1
+                        new_grid[i][j] = grid[i][j] - 1
+
+            if count_bottom:
+                total_count += bottom * 2
+                count_bottom = False
+
+            grid = new_grid
+
+        return total_count
+
