5a579e88e885eef0792e8eff9428d1d7aa5a8dce
vyshor <vyshornehc@gmail.com>
2023-09-28 19:53:18 +0800

    Add Trivial Solution

--- Sort Array By Parity.py
+++ Sort Array By Parity.py
@@ -1,13 +1,17 @@
 class Solution:
-    def sortArrayByParity(self, A: List[int]) -> List[int]:
-        even = []
-        odd = []
-        for n in A:
-            if n % 2:
-                odd.append(n)
-            else:
-                even.append(n)
-        return even + odd
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        return sorted(nums, key=lambda x: x % 2)
 
-    # Time: O(n)
-    # Space: O(n)
+# class Solution:
+#     def sortArrayByParity(self, A: List[int]) -> List[int]:
+#         even = []
+#         odd = []
+#         for n in A:
+#             if n % 2:
+#                 odd.append(n)
+#             else:
+#                 even.append(n)
+#         return even + odd
+#
+#     # Time: O(n)
+#     # Space: O(n)
