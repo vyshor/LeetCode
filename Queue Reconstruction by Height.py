ee182cf4763e21f2fbb0e95bae91ad8c878b0330
vyshor <vyshornehc@gmail.com>
2023-10-02 01:00:40 +0800

    Add Bigbrain solu

--- Queue Reconstruction by Height.py
+++ Queue Reconstruction by Height.py
@@ -1,23 +1,31 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        n = len(people)
-        choices = [(person[1], person[0], person[1]) for person in people]
-        heapq.heapify(choices)
-        q = []
+        people.sort(key=lambda p: (-p[0], p[1]))
+        ans = []
+        for p in people:
+            ans.insert(p[1], p)
+        return ans
 
-        while choices:
-            _, h, k = choices[0]
-            m = len(choices)
-            q.append((h, k))
-
-            if len(q) == n:
-                return q
-
-            new_choices = []
-            for i in range(1, m):
-                remaining, old_h, old_k = choices[i]
-                if h >= old_h:
-                    remaining -= 1
-                new_choices.append((remaining, old_h, old_k))
-            heapq.heapify(new_choices)
-            choices = new_choices
+# class Solution:
+#     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+#         n = len(people)
+#         choices = [(person[1], person[0], person[1]) for person in people]
+#         heapq.heapify(choices)
+#         q = []
+#
+#         while choices:
+#             _, h, k = choices[0]
+#             m = len(choices)
+#             q.append((h, k))
+#
+#             if len(q) == n:
+#                 return q
+#
+#             new_choices = []
+#             for i in range(1, m):
+#                 remaining, old_h, old_k = choices[i]
+#                 if h >= old_h:
+#                     remaining -= 1
+#                 new_choices.append((remaining, old_h, old_k))
+#             heapq.heapify(new_choices)
+#             choices = new_choices
