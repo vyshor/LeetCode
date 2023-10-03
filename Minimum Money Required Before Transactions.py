dbc2b8f3da75506598ad64f53f075f29b3986fd2
vyshor <vyshornehc@gmail.com>
2023-10-03 22:30:34 +0800

    Add more optimized solution

--- Minimum Money Required Before Transactions.py
+++ Minimum Money Required Before Transactions.py
@@ -1,27 +1,36 @@
 class Solution:
     def minimumMoney(self, transactions: List[List[int]]) -> int:
-        losses, gains = [], []
+        sum_lose = 0
+        additional = 0
         for cost, cashback in transactions:
-            nett_gain = cashback - cost
-            if nett_gain > 0:
-                gains.append((-cost))
-            else:
-                losses.append((cashback, -cost, -cost + cashback))
+            sum_lose += max(0, cost - cashback)
+            additional = max(additional, min(cost, cashback))
+        return sum_lose + additional
 
-        losses.sort()
-        heapq.heapify(gains)
-
-        if not gains:
-            gains = [0]
-
-        # The more negative the nett_gain the earlier
-        # The larger the cashback the later it should be
-
-        minn = 0
-        net_state = 0
-
-        for (_, cost, nett_gain) in losses:
-            minn = min(minn, net_state + cost)
-            net_state += nett_gain
-
-        return -min(minn, net_state + gains[0])
+# class Solution:
+#     def minimumMoney(self, transactions: List[List[int]]) -> int:
+#         losses, gains = [], []
+#         for cost, cashback in transactions:
+#             nett_gain = cashback - cost
+#             if nett_gain > 0:
+#                 gains.append((-cost))
+#             else:
+#                 losses.append((cashback, -cost, -cost + cashback))
+#
+#         losses.sort()
+#         heapq.heapify(gains)
+#
+#         if not gains:
+#             gains = [0]
+#
+#         # The more negative the nett_gain the earlier
+#         # The larger the cashback the later it should be
+#
+#         minn = 0
+#         net_state = 0
+#
+#         for (_, cost, nett_gain) in losses:
+#             minn = min(minn, net_state + cost)
+#             net_state += nett_gain
+#
+#         return -min(minn, net_state + gains[0])
