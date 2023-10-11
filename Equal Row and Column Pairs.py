98c82ca1edf762469f43c3ce2b9ad3917a0b6f02
vyshor <vyshornehc@gmail.com>
2023-10-11 16:17:18 +0800

    Add more optimized solution

--- Equal Row and Column Pairs.py
+++ Equal Row and Column Pairs.py
@@ -2,17 +2,42 @@ class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         n = len(grid)
         m = len(grid[0])
-
-        def compare(row, col):
-            vals = grid[row]
-            for i, val in enumerate(vals):
-                if grid[i][col] != val:
-                    return False
-            return True
+        dp = {}
 
         count = 0
         for i in range(n):
-            for j in range(m):
-                count += int(compare(i, j))
+            key = str(grid[i])
+            if key not in dp:
+                dp[key] = 1
+            else:
+                dp[key] += 1
+
+        for j in range(m):
+            arr = []
+            for i in range(n):
+                arr.append(grid[i][j])
+
+            key = str(arr)
+            if key in dp:
+                count += dp[key]
 
         return count
+
+# class Solution:
+#     def equalPairs(self, grid: List[List[int]]) -> int:
+#         n = len(grid)
+#         m = len(grid[0])
+#
+#         def compare(row, col):
+#             vals = grid[row]
+#             for i, val in enumerate(vals):
+#                 if grid[i][col] != val:
+#                     return False
+#             return True
+#
+#         count = 0
+#         for i in range(n):
+#             for j in range(m):
+#                 count += int(compare(i, j))
+#
+#         return count
