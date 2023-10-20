583e72c5acf88223b8300725839283a141240cc3
vyshor <vyshornehc@gmail.com>
2023-10-20 20:21:43 +0800

    Add more optimized solution

--- Flatten Nested List Iterator.py
+++ Flatten Nested List Iterator.py
@@ -22,26 +22,51 @@
 
 class NestedIterator:
     def __init__(self, nestedList: [NestedInteger]):
-        self.dp = self.unravel(nestedList)
-        self.pt = 0
-
-    def unravel(self, nestedList):
-        flatten = []
-        for nested in nestedList:
-            if nested.isInteger():
-                flatten.append(nested.getInteger())
-            else:
-                flatten += self.unravel(nested.getList())
-        return flatten
+        self.q = deque(nestedList)
+        while self.q and not self.q[0].isInteger():
+            v = self.q.popleft()
+            _list = v.getList()
+            n = len(_list)
+            for i in range(n - 1, -1, -1):
+                self.q.appendleft(_list[i])
 
     def next(self) -> int:
-        num = self.dp[self.pt]
-        self.pt += 1
-        return num
+        v = self.q.popleft()
+        while self.q and not self.q[0].isInteger():
+            v2 = self.q.popleft()
+            _list = v2.getList()
+            n = len(_list)
+            for i in range(n - 1, -1, -1):
+                self.q.appendleft(_list[i])
+        return v.getInteger()
 
     def hasNext(self) -> bool:
-        return self.pt < len(self.dp)
+        return bool(self.q)
+
 
 # Your NestedIterator object will be instantiated and called as such:
 # i, v = NestedIterator(nestedList), []
 # while i.hasNext(): v.append(i.next())
+
+# class NestedIterator:
+#     def __init__(self, nestedList: [NestedInteger]):
+#         self.dp = self.unravel(nestedList)
+#         self.pt = 0
+#
+#     def unravel(self, nestedList):
+#         flatten = []
+#         for nested in nestedList:
+#             if nested.isInteger():
+#                 flatten.append(nested.getInteger())
+#             else:
+#                 flatten += self.unravel(nested.getList())
+#         return flatten
+#
+#     def next(self) -> int:
+#         num = self.dp[self.pt]
+#         self.pt += 1
+#         return num
+#
+#     def hasNext(self) -> bool:
+#         return self.pt < len(self.dp)
+
