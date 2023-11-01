a8a81e617e92ff706f287252e3b6cda3ea94d903
vyshor <vyshornehc@gmail.com>
2023-11-01 15:17:26 +0800

    Add more space optimized solution

--- Find Mode in Binary Search Tree.py
+++ Find Mode in Binary Search Tree.py
@@ -6,25 +6,58 @@
 #         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        counter = {}
+        minn_val = float('-inf')
+        count = 0
+        maxx = 0
+        mode = []
 
         def exploreNode(node):
-            nonlocal counter
+            nonlocal count, minn_val, maxx, mode
+
             if node is None:
                 return
 
-            if node.val not in counter:
-                counter[node.val] = 1
-            else:
-                counter[node.val] += 1
-
             exploreNode(node.left)
+
+            if node.val == minn_val:
+                count += 1
+            else:
+                minn_val = node.val
+                count = 1
+
+            if count > maxx:
+                maxx = count
+                mode = [node.val]
+            elif count == maxx:
+                mode.append(node.val)
+
             exploreNode(node.right)
 
         exploreNode(root)
-        maxx = max(counter.values())
-        mode = []
-        for v, count in counter.items():
-            if count == maxx:
-                mode.append(v)
+
         return mode
+
+# class Solution:
+#     def findMode(self, root: Optional[TreeNode]) -> List[int]:
+#         counter = {}
+#
+#         def exploreNode(node):
+#             nonlocal counter
+#             if node is None:
+#                 return
+#
+#             if node.val not in counter:
+#                 counter[node.val] = 1
+#             else:
+#                 counter[node.val] += 1
+#
+#             exploreNode(node.left)
+#             exploreNode(node.right)
+#
+#         exploreNode(root)
+#         maxx = max(counter.values())
+#         mode = []
+#         for v, count in counter.items():
+#             if count == maxx:
+#                 mode.append(v)
+#         return mode
