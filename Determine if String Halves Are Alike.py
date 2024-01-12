ca9bd0abe7ae625be7a1403f217a5b167f32c6de
vyshor <vyshornehc@gmail.com>
2024-01-12 23:07:41 +0800

    Add more optimized solution

--- Determine if String Halves Are Alike.py
+++ Determine if String Halves Are Alike.py
@@ -1,16 +1,29 @@
-from collections import Counter
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
-        a_s, b_s = Counter(s[:len(s)//2]), Counter(s[len(s)//2:])
-        a_c, b_c = 0, 0
-        for c in a_s:
-            if c in vowels:
-                a_c += a_s[c]
-        for c in b_s:
-            if c in vowels:
-                b_c += b_s[c]
-        return a_c == b_c
+        n = len(s)
+        vowels = set('aeiouAEIOU')
+        count = 0
+        for i in range(n):
+            if s[i] in vowels:
+                if i < n // 2:
+                    count += 1
+                else:
+                    count -= 1
+        return count == 0
+
+# from collections import Counter
+# class Solution:
+#     def halvesAreAlike(self, s: str) -> bool:
+#         vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+#         a_s, b_s = Counter(s[:len(s)//2]), Counter(s[len(s)//2:])
+#         a_c, b_c = 0, 0
+#         for c in a_s:
+#             if c in vowels:
+#                 a_c += a_s[c]
+#         for c in b_s:
+#             if c in vowels:
+#                 b_c += b_s[c]
+#         return a_c == b_c
 
 # Time: O(n)
 # Space: O(1)
