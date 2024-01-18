59d3240e82dcf9b733679b4925fa7c7fd4a75692
vyshor <vyshornehc@gmail.com>
2024-01-18 21:59:06 +0800

    Add optimized Climbing Stairs

--- Climbing Stairs.py
+++ Climbing Stairs.py
@@ -1,13 +1,20 @@
-import math as m
 class Solution:
     def climbStairs(self, n: int) -> int:
-        total = 0
-        a = 0
-        while n-a>=0:
-            total += int(m.factorial(n)/(m.factorial(a) * m.factorial(n-a)))
-            a += 1
-            n -= 1
-        return total
+        t_2, t_1 = 0, 1
+        for i in range(n):
+            t_2, t_1 = t_1, t_2+t_1
+        return t_1
+
+# import math as m
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         total = 0
+#         a = 0
+#         while n-a>=0:
+#             total += int(m.factorial(n)/(m.factorial(a) * m.factorial(n-a)))
+#             a += 1
+#             n -= 1
+#         return total
 
 # Time: O(n^2) # Assuming factorial is O(n) # Otherwise O(n) if factorial calculation is O(1)
 # Space: O(1)
