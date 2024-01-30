2c4fa3a81a765eed35e58e6540ed049c03c27ee2
vyshor <vyshornehc@gmail.com>
2024-01-30 20:00:47 +0800

    Add more optimized solution

--- Evaluate Reverse Polish Notation.py
+++ Evaluate Reverse Polish Notation.py
@@ -1,6 +1,18 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
+        op = {"+": operator.add, "-": operator.sub, "*": operator.mul, "/":lambda x, y: int(operator.truediv(x, y))}
+        for token in tokens:
+            if token in op:
+                num2, num1 = stack.pop(), stack.pop()
+                stack.append(op[token](num1, num2))
+            else:
+                stack.append(int(token))
+        return stack[0]
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
         for token in tokens:
             if token == "+":
                 right, left = int(stack.pop()), int(stack.pop())
