7b04e4b041805709ff97c5a5edf9510f4e5b6a94
vyshor <vyshornehc@gmail.com>
2024-02-08 22:00:40 +0800

    Add Perfect Squares

--- Perfect Squares.py
+++ Perfect Squares.py
@@ -1,17 +1,37 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        dp = [n] * (n + 1)
+        arr = []
+        for i in range(n + 1):
+            sq = i * i
+            if sq == n:
+                return 1
+            elif sq > n:
+                break
+            arr.append(sq)
+
+        dp = [float('inf')] * (n + 1)
         dp[-1] = 0
-
-        primes = []
-        for i in range(1, int(math.sqrt(n)) + 1):
-            primes.append(i ** 2)
-
-        for prime in primes:
-            for i in range(n, 0, -1):
-                if prime > i:
+        for num in arr:
+            for i in range(n, -1, -1):
+                if i - num < 0:
                     break
-                dp[i - prime] = min(dp[i - prime], dp[i] + 1)
-
-        # print(dp)
+                dp[i - num] = min(dp[i - num], dp[i] + 1)
         return dp[0]
+
+# class Solution:
+#     def numSquares(self, n: int) -> int:
+#         dp = [n] * (n + 1)
+#         dp[-1] = 0
+#
+#         primes = []
+#         for i in range(1, int(math.sqrt(n)) + 1):
+#             primes.append(i ** 2)
+#
+#         for prime in primes:
+#             for i in range(n, 0, -1):
+#                 if prime > i:
+#                     break
+#                 dp[i - prime] = min(dp[i - prime], dp[i] + 1)
+#
+#         # print(dp)
+#         return dp[0]
