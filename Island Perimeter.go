1638c14ec34103ba85d413ea8fef9d7b70c62b03
vyshor <vyshornehc@gmail.com>
2024-04-18 22:45:33 +0800

    Add updated Island Perimeter

--- Island Perimeter.go
+++ Island Perimeter.go
@@ -1,59 +1,87 @@
-func getKey(i, j int) int {
-    return (i << 10) | j
-}
-
-func returnKey(key int) (int, int) {
-    i := key >> 10
-    j := ((1 << 10) - 1) & key
-    return i, j
-}
-
 func islandPerimeter(grid [][]int) int {
-    visited := make(map[int]interface{})
     var perimeter int
     n := len(grid)
     m := len(grid[0])
-    q := make([]int, 0)
 
-    to_break := false
     for i := 0; i < n; i++ {
+        prev := 0
         for j := 0; j < m; j++ {
-            if grid[i][j] == 1 {
-                key := getKey(i, j)
-                q = append(q, key)
-                to_break = true
-                break
-            }
+            perimeter += grid[i][j] ^ prev
+            prev = grid[i][j]
         }
-
-        if to_break {
-            break
-        }
+        perimeter += prev
     }
 
-    for len(q) > 0 {
-        key := q[len(q)-1]
-        q = q[:len(q)-1]
-        if _, ok := visited[key]; ok {
-            continue
-        }
 
-        visited[key] = nil
-        i, j := returnKey(key)
-        keys := []int{getKey(i-1, j), getKey(i+1, j), getKey(i, j-1), getKey(i, j+1)}
-        for _, new_key := range keys {
-            if _, ok := visited[new_key]; ok {
-                continue
-            }
-            x, y := returnKey(new_key)
-            if 0 <= x && x < n && 0 <= y && y < m && grid[x][y] == 1 {
-                q = append(q, new_key)
-            } else {
-                perimeter++
-            }
+    for j := 0; j < m; j++ {
+        prev := 0
+        for i := 0; i < n; i++ {
+            perimeter += grid[i][j] ^ prev
+            prev = grid[i][j]
         }
-
+        perimeter += prev
     }
+
     return perimeter
 }
+
+
+// func getKey(i, j int) int {
+//     return (i << 10) | j
+// }
+//
+// func returnKey(key int) (int, int) {
+//     i := key >> 10
+//     j := ((1 << 10) - 1) & key
+//     return i, j
+// }
+//
+// func islandPerimeter(grid [][]int) int {
+//     visited := make(map[int]interface{})
+//     var perimeter int
+//     n := len(grid)
+//     m := len(grid[0])
+//     q := make([]int, 0)
+//
+//     to_break := false
+//     for i := 0; i < n; i++ {
+//         for j := 0; j < m; j++ {
+//             if grid[i][j] == 1 {
+//                 key := getKey(i, j)
+//                 q = append(q, key)
+//                 to_break = true
+//                 break
+//             }
+//         }
+//
+//         if to_break {
+//             break
+//         }
+//     }
+//
+//     for len(q) > 0 {
+//         key := q[len(q)-1]
+//         q = q[:len(q)-1]
+//         if _, ok := visited[key]; ok {
+//             continue
+//         }
+//
+//         visited[key] = nil
+//         i, j := returnKey(key)
+//         keys := []int{getKey(i-1, j), getKey(i+1, j), getKey(i, j-1), getKey(i, j+1)}
+//         for _, new_key := range keys {
+//             if _, ok := visited[new_key]; ok {
+//                 continue
+//             }
+//             x, y := returnKey(new_key)
+//             if 0 <= x && x < n && 0 <= y && y < m && grid[x][y] == 1 {
+//                 q = append(q, new_key)
+//             } else {
+//                 perimeter++
+//             }
+//         }
+//
+//     }
+//     return perimeter
+// }
 
--- Island Perimeter.py
+++ Island Perimeter.py
@@ -1,33 +1,54 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        visited = set()
         perimeter = 0
         n = len(grid)
         m = len(grid[0])
-        q = []
 
-        to_break = False
         for i in range(n):
+            prev_occupied = 0
             for j in range(m):
-                if grid[i][j]:
-                    q.append((i, j))
-                    to_break = True
-                    break
-            if to_break:
-                break
+                perimeter += grid[i][j] ^ prev_occupied
+                prev_occupied = grid[i][j]
+            perimeter += prev_occupied
 
-        while q:
-            i, j = q.pop()
-            if (i, j) in visited:
-                continue
-
-            visited.add((i, j))
-            for (x, y) in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
-                if (x, y) in visited:
-                    continue
-
-                if 0 <= x < n and 0 <= y < m and grid[x][y]:
-                    q.append((x, y))
-                else:
-                    perimeter += 1
+        for j in range(m):
+            prev_occupied = 0
+            for i in range(n):
+                perimeter += grid[i][j] ^ prev_occupied
+                prev_occupied = grid[i][j]
+            perimeter += prev_occupied
         return perimeter
+
+# class Solution:
+#     def islandPerimeter(self, grid: List[List[int]]) -> int:
+#         visited = set()
+#         perimeter = 0
+#         n = len(grid)
+#         m = len(grid[0])
+#         q = []
+#
+#         to_break = False
+#         for i in range(n):
+#             for j in range(m):
+#                 if grid[i][j]:
+#                     q.append((i, j))
+#                     to_break = True
+#                     break
+#             if to_break:
+#                 break
+#
+#         while q:
+#             i, j = q.pop()
+#             if (i, j) in visited:
+#                 continue
+#
+#             visited.add((i, j))
+#             for (x, y) in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+#                 if (x, y) in visited:
+#                     continue
+#
+#                 if 0 <= x < n and 0 <= y < m and grid[x][y]:
+#                     q.append((x, y))
+#                 else:
+#                     perimeter += 1
+#         return perimeter
