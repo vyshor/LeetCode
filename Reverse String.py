5dd12d276982026b22b6f68ba4e91858c3b76d56
vyshor <vyshornehc@gmail.com>
2024-06-02 11:43:19 +0800

    Add Reverse String

--- Reverse String.py
+++ Reverse String.py
@@ -3,9 +3,21 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        n = len(s)
-        start, end = 0, n - 1
-        while start < end:
-            s[start], s[end] = s[end], s[start]
-            start += 1
-            end -= 1
+        left, right = 0, len(s)-1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+#         n = len(s)
+#         start, end = 0, n - 1
+#         while start < end:
+#             s[start], s[end] = s[end], s[start]
+#             start += 1
+#             end -= 1
