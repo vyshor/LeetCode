5c6a3b0ddbc5225f3ed6fec63c2de512db4a4b80
vyshor <vyshornehc@gmail.com>
2025-09-28 14:53:35 +0800

    Add another Triangle Number

--- Valid Triangle Number.cpp
+++ Valid Triangle Number.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int k;
+        int count = 0;
+        for (int i = 0; i < n-2; i++) {
+            if (nums[i] == 0) continue;
+            k = i+2;
+            for (int j = i+1;j < n-1; j++) {
+                while (k < n && nums[k] < nums[i] + nums[j]) {
+                    k++;
+                }
+                count += k - j - 1;
+                // cout << "j: " << j << " k: " << k << " count: " << count << endl;
+            }
+        }
+        return count;
+    }
+};
+
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
         int total_count = nums.size();
         vector<int> uni_num;
         unordered_map<int, int> counter;
