ebb39511d1fd57149c76c21462f1d9edfce22651
vyshor <vyshornehc@gmail.com>
2025-10-01 11:24:47 +0800

    Add Water Bottles alternative

--- Water Bottles.cpp
+++ Water Bottles.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
+        int count = numBottles;
+        while (numBottles >= numExchange) {
+            int mul = numBottles / numExchange;
+            int val = mul * numExchange;
+            count += mul;
+            numBottles += mul - val ;
+        }
+        return count;
+    }
+};
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
         int count = 0, remainder = 0;
         while (numBottles) {
             count += numBottles;
