8bd2a952f99315f77073400f19dff4503148a8ff
vyshor <vyshornehc@gmail.com>
2025-10-07 01:30:15 +0800

    Add another way for base 7

--- Base 7.cpp
+++ Base 7.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     string convertToBase7(int num) {
+        if (num == 0) return "0";
+        bool neg = (num < 0);
+        if (neg) num = -num;
+        stringstream ss;
+        while (num > 0) {
+            int r = num % 7;
+            ss << to_string(r);
+            num /= 7;
+        }
+        if (neg) ss << "-";
+        string s = ss.str();
+        reverse(s.begin(), s.end());
+        return s;
+    }
+};
+
+class Solution {
+public:
+    string convertToBase7(int num) {
         stringstream ss;
         if (num < 0) {
             num = -num;
