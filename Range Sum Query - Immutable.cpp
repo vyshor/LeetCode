60607a6b2c0891e16390166cba808ae0d5f947e0
vyshor <vyshornehc@gmail.com>
2025-10-12 00:50:44 +0800

    Add Range Sum Query

---
+++ Range Sum Query - Immutable.cpp
@@ -0,0 +1,22 @@
+class NumArray {
+public:
+    vector<int> prefix;
+
+    NumArray(vector<int>& nums) {
+        int n = nums.size();
+        prefix = vector<int>(n+1, 0);
+        for (int i = 0; i < n; i++) {
+            prefix[i+1] = prefix[i] + nums[i];
+        }
+    }
+
+    int sumRange(int left, int right) {
+        return prefix[right+1]-prefix[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
