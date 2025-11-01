6524fc23ffe2074338c7798a6aacb621da68e3e1
vyshor <vyshornehc@gmail.com>
2025-11-01 21:31:34 +0800

    Add Minimum Score of a Path Between 2 Cities

---
+++ Minimum Score of a Path Between Two Cities.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<int> parents(n+1);
+        vector<int> minn(n+1, INT_MAX);
+        std::iota(parents.begin(), parents.end(), 0);
+
+        function<int(int i)> ffind;
+        ffind = [&parents, &ffind] (int i) -> int {
+            if (parents[i] == i) return i;
+            int parent = ffind(parents[i]);
+            parents[i] = parent;
+            return parent;
+        };
+
+        auto uni = [&parents, &minn, &ffind] (int i, int j) {
+            int parent_i = ffind(i);
+            int parent_j = ffind(j);
+            if (parent_i != parent_j) {
+                parents[parent_j] = parent_i;
+                minn[parent_i] = min(minn[parent_i], minn[parent_j]);
+            }
+        };
+
+        for (auto& road: roads) {
+            int a = road[0], b = road[1], dist = road[2];
+            int parent_a = ffind(a), parent_b = ffind(b);
+
+            minn[parent_a] = min(minn[parent_a], dist);
+            minn[parent_b] = min(minn[parent_b], dist);
+            uni(parent_a, parent_b);
+        }
+        return minn[ffind(1)];
+    }
+};
