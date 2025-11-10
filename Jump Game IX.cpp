b4c0aec9487a1638fc873c6c781682c60187b542
vyshor <vyshornehc@gmail.com>
2025-11-10 21:04:27 +0800

    Add Jump Game 9

---
+++ Jump Game IX.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> maxValue(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> parents(n);
+        std::iota(parents.begin(), parents.end(), 0);
+
+        function<int(int)> find;
+        find = [&find, &parents] (int i) -> int{
+            if (parents[i] == i) return i;
+            int parent = find(parents[i]);
+            parents[i] = parent;
+            return parent;
+        };
+
+        auto uni = [&find, &parents] (int i, int j) {
+            int parent_i = find(i);
+            int parent_j = find(j);
+            if (parent_i == parent_j) return;
+
+            parents[parent_j] = parent_i;
+        };
+
+        vector<pair<int, int>> stack = {{nums[0], 0}};
+        for (int i = 1; i < n; ++i) {
+            int num = nums[i];
+            auto& top_stack = stack.back();
+            stack.pop_back();
+
+            while (stack.size() > 0 && stack.back().first > num) {
+                uni(i, stack.back().second);
+                stack.pop_back();
+            }
+
+            if (num < top_stack.first) {
+                uni(top_stack.second, i);
+                stack.push_back(std::move(top_stack));
+            } else {
+                stack.push_back(std::move(top_stack));
+                stack.emplace_back(nums[i], i);
+            }
+        }
+
+        vector<int> ans(n);
+        for (int i = 0; i < n; ++i) {
+            ans[i] = nums[find(i)];
+        }
+        return ans;
+    }
+};
