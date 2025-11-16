c56509f128412e8be9661d44175d367e1f105156
vyshor <vyshornehc@gmail.com>
2025-11-16 13:45:25 +0800

    Add Number of Zero Filled Subarrays

---
+++ Number of Zero-Filled Subarrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long ans = 0;
+        long long count = 0;
+        for (int num: nums) {
+            if (num == 0) {
+                ++count;
+            } else {
+                ans += (count*(count+1)/2);
+                count = 0;
+            }
+        }
+        ans += (count*(count+1)/2);
+        return ans;
+    }
+};
