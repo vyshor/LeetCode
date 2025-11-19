f4edb9b75ede3906142679a4c8cb7cba6655e82e
vyshor <vyshornehc@gmail.com>
2025-11-19 10:56:57 +0800

    Add FinalValue

--- Keep Multiplying Found Values by Two.cpp
+++ Keep Multiplying Found Values by Two.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
+        vector<int> seen(1001, 0);
+        for (int num: nums) seen[num] = 1;
+        while (original <= 1000 && seen[original]) {
+            original *= 2;
+        }
+        return original;
+    }
+};
+
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
         set<int> seen(nums.begin(), nums.end());
         while (seen.contains(original)) {
             original *= 2;
