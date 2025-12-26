ee3a54728a69227d954278a74bb2c23830b0395c
vyshor <vyshornehc@gmail.com>
2025-12-26 16:56:47 +0800

    Add alt sol for min penalty for a shop

--- Minimum Penalty for a Shop.cpp
+++ Minimum Penalty for a Shop.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
+        int opened{0}, closed{0};
+        int t{0};
+        int i{0};
+        for (char cust: customers) {
+            if (cust == 'Y') {
+                ++closed;
+                if (opened + 1 < closed) {
+                    t = i;
+                    closed = opened+1;
+                }
+            } else {
+                ++opened;
+                if (opened-1 < closed) {
+                    t = i;
+                    closed = opened-1;
+                }
+            }
+            ++i;
+        }
+        if (opened < closed) return customers.size();
+        return t;
+    }
+};
+
+class Solution {
+public:
+    int bestClosingTime(string customers) {
         int opened = 0, closed = 0, j = 0, n = customers.size();
         for (int i = 0; i < n; i++) {
             bool coming = customers[i] == 'Y';
