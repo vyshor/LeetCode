38485f4017b471143059bd52f055c9ec31fa91e6
vyshor <vyshornehc@gmail.com>
2025-12-27 23:54:18 +0800

    ADd alt solution to Meeting Rooms 3

--- Meeting Rooms III.cpp
+++ Meeting Rooms III.cpp
@@ -1,6 +1,67 @@
 class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
+        vector<int> rooms_count(n, 0);
+        vector<int> available(n);
+
+        std::iota(available.begin(), available.end(), 0);
+        std::make_heap(available.begin(), available.end(), std::greater<>{});
+
+        vector<tuple<int64_t, int, int, int64_t>> q; // Ts, is_start, room, duration
+        deque<pair<int64_t, int64_t>> pending_meetings;
+
+        for (auto& meeting: meetings) {
+            q.emplace_back(meeting[0], 1, -1, meeting[1]-meeting[0]);
+        }
+        std::make_heap(q.begin(), q.end(), std::greater<>{});
+
+        while (q.size() > 0) {
+            std::pop_heap(q.begin(), q.end(), std::greater<>{});
+            auto [ts, is_start, room, duration] = q.back();
+            q.pop_back();
+
+            if (is_start) {
+                // Meeting start
+                pending_meetings.emplace_back(ts, duration);
+            } else {
+                // Meeting end
+                available.push_back(room);
+                std::push_heap(available.begin(), available.end(), std::greater<>{});
+
+            }
+
+            while (pending_meetings.size() > 0 && available.size() > 0) {
+                std::pop_heap(available.begin(), available.end(), std::greater<>{});
+                int room = available.back();
+                available.pop_back();
+
+                ++rooms_count[room];
+
+                auto [_, meeting_duration] = pending_meetings.front();
+                pending_meetings.pop_front();
+
+                q.emplace_back(ts+meeting_duration, 0, room, -1);
+                std::push_heap(q.begin(), q.end(), std::greater<>{});
+            }
+        }
+
+        int maxx{-1};
+        int idx{0};
+        for (int i{0}; i < n; ++i) {
+            if (rooms_count[i] > maxx) {
+                maxx = rooms_count[i];
+                idx = i;
+            }
+        }
+
+        return idx;
+    }
+};
+
+
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
         int negative_shift = -100;
         vector<int> uses(n, 0);
         priority_queue<int, vector<int>, greater<int>> available;
