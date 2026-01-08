490ea10c58a85ce246557962fa52bc239eefedf6
vyshor <vyshornehc@gmail.com>
2026-01-09 02:10:43 +0800

    Add Set version of LRU cache

--- LRU Cache.cpp
+++ LRU Cache.cpp
@@ -48,3 +48,48 @@ public:
  * obj->put(key,value);
  */
 
+class LRUCache {
+public:
+    int cap{0};
+    int counter{0};
+    std::unordered_map<int, pair<int, int>> mapping; // Key -> fval, val
+    std::set<pair<int, int>> usages; // fval, key
+
+    LRUCache(int capacity) : cap(capacity) {
+    }
+
+    int get(int key) {
+        if (mapping.contains(key)) {
+            int value = mapping[key].second;
+            usages.erase(make_pair(mapping[key].first, key));
+            int fval = ++counter;
+            mapping[key] = std::make_pair(fval, value);
+            usages.emplace(fval, key);
+            return mapping[key].second;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if (!mapping.contains(key)) {
+            if (mapping.size() == cap) {
+                auto smallest_pair = *usages.begin();
+                usages.erase(smallest_pair);
+                mapping.erase(smallest_pair.second);
+            }
+        } else {
+            usages.erase(make_pair(mapping[key].first, key));
+        }
+
+        int fval = ++counter;
+        mapping[key] = std::make_pair(fval, value);
+        usages.emplace(fval, key);
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
